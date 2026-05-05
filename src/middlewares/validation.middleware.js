36b1730cdbd0cc38f6cb7ee4cf6066ec7ab8bdba
Disha <your-email@example.com>
2026-05-05 10:19:17 +0530

    update

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,16 +1,16 @@
-const validate = (schema, property = 'body') => {
+const validate = (schema, property = "body") => {
   return (req, res, next) => {
     const { error } = schema.validate(req[property]);
-    
+
     if (error) {
       const errorMessage = error.details[0].message;
       return res.status(400).json({
         success: false,
         message: errorMessage,
-        data: null
+        data: null,
       });
     }
-    
+
     next();
   };
 };
